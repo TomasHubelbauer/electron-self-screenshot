e0312e719486c8ceddb1ce04e7433a198281dddd
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-07-08 09:15:29 +0200

    Add an example from the rendered process

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const { app, BrowserWindow } = require('electron');
 
 app.on('ready', launchInfo => {
   const window = new BrowserWindow({ width: 800, height: 600 });
-  window.webContents.loadFile(__dirname + './index.html');
+  // https://github.com/electron/electron/issues/1602
+  window.webContents.loadURL('http://hubelbauer.net/');
   setTimeout(() => {
-    window.webContents.capturePage(image => {
+    window.capturePage(image => {
       const dataUrl = image.toDataURL();
       window.webContents.loadURL(dataUrl);
     });
   }, 1000);
 });
+
+// Or from the renderer process in index.js/DevTools:
+/*
+{
+  const electron = require('electron');
+  electron.remote.getCurrentWindow().capturePage(image => {
+    //electron.clipboard.writeImage(image);
+    electron.clipboard.writeText(image.toDataURL());
+    console.log('Data URL is in clipboard.');
+  });
+}
+*/
