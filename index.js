4ed2a3b3ad6bd8688425309b6f5f2078902adef0
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-07-08 10:59:42 +0200

    Demo using Electron EdgeJS for screenshot

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const { app, BrowserWindow } = require('electron');
+const fs = require('fs');
+const screenshot = require('window-screenshot');
 
 app.on('ready', launchInfo => {
   const window = new BrowserWindow({ width: 800, height: 600 });
   // https://github.com/electron/electron/issues/1602
   window.webContents.loadURL('http://hubelbauer.net/');
   setTimeout(() => {
-    window.capturePage(image => {
-      const dataUrl = image.toDataURL();
-      window.webContents.loadURL(dataUrl);
-    });
-  }, 1000);
+    // window.capturePage(image => {
+    //   const dataUrl = image.toDataURL();
+    //   window.webContents.loadURL(dataUrl);
+    // });
+    screenshot(
+      0, // 0 is for active window
+      function (err, res) {
+        if (err) throw err;
+
+        fs.writeFile("result.png", res);
+      });
+  }, 5000);
 });
 
 // Or from the renderer process in index.js/DevTools:
